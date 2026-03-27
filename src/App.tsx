3e63497a3267424587bea76e11f104c4e88495b4
Phillip Thapa <1thapat@gmail.com>
2026-03-27 09:36:25 -0400

    make a state for catogories and populated it with the use of useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,21 @@ import './App.css'
 import Home from './pages/Home'
 import Navbar from './components/Navbar'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Favorites from './pages/Favorites'
+import Category from './pages/Category'
+import RecipeDetail from './pages/RecipeDetail'
 
 
 function App() {
   return (
-   
+  
     <BrowserRouter> 
     <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="/favorites" element={<Favorites/>} />
+        <Route path="/category/:name" element={<Category/>} />
+        <Route path="/recipe/:id" element={<RecipeDetail/>} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,19 @@
+import { useEffect, useState } from "react";
+
 function Home() {
+ 
+    const [categories, setCategories] = useState<any[]>([]);
+
+    useEffect( () => {
+        fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
+        .then(res=>res.json())
+        .then(data => {
+            setCategories(data.categories)
+        })
+    }, [])
+
+    console.log(categories);
+
   return (
     <div>
       <h1>Home Page</h1>
