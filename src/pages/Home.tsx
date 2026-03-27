de64c6ff9d653d19ee6c59089886779a0a162f26
Phillip Thapa <1thapat@gmail.com>
2026-03-27 10:22:39 -0400

    added error handling and loading message in home page when fetch goes wrong

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,38 @@
 import { useEffect, useState } from "react";
 
 function Home() {
- 
-    const [categories, setCategories] = useState<any[]>([]);
+  const [categories, setCategories] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
 
-    useEffect( () => {
-        fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
-        .then(res=>res.json())
-        .then(data => {
-            setCategories(data.categories)
-        })
-    }, [])
+  useEffect(() => {
+    fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("failed to fetch data");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setCategories(data.categories);
+        setLoading(false);
+      }).catch((error) =>{
+        setError(error.message)
+        setLoading(false)
+      });
+  }, []);
 
-    console.log(categories);
+  console.log(categories);
+
+  if(loading) return <p>loading</p>
+  if(error) return <p>Error: {error} </p>;
 
   return (
     <div>
-        {categories.map((categories) => (
-            <p key={categories.idCategory}> {categories.strCategory}</p>
-        ))}
+      {categories.map((categories) => (
+        <p key={categories.idCategory}> {categories.strCategory}</p>
+      ))}
     </div>
   );
 }
-
 export default Home;
